288e1667dca6f7a949ddea4e9158265ceb5556e2
Vili <vili.hatonen@helsinki.fi>
2019-01-25 23:13:36 +0200

    2.3

--- osa2/2.1kurssien_sisalto/src/index.js
+++ osa2/2.1kurssien_sisalto/src/index.js
@@ -1,72 +1,101 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
+const Header = ({name}) => {
+    console.log(name);
+    
     return (
         <div>
-            <h1>{props.course}</h1>
+            <h1>{name}</h1>
         </div>
     )
 }
 
-const Part = (props) => {
+const Part = ({part}) => {
+    console.log(part);
+    
     return (
         <div>
             <p>
-                {props.part.name} {props.part.exercises}
+                {part.name} {part.exercises}
             </p>
         </div>
     )
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
+    console.log(parts);
+    
+    const rows = () => parts.map(p =>
+        <Part key={p.id} part={p} />
+    )
+
     return (
         <div>
-            <Part part={props.parts[0]} />
-            <Part part={props.parts[1]} />
-            <Part part={props.parts[2]} />
+            {rows()}
         </div>
     )
 }
 
-const Total = (props) => {
+const Total = ({parts}) => {
+    console.log(parts);
+    
+    const reducer = (s, p) => {
+        console.log("at Total: ", s, p);
+        return s + p.exercises
+    }
+  
     return (
         <div>
-            <p>yhteensä {props.parts[0].exercises + 
-                props.parts[1].exercises + 
-                props.parts[2].exercises} tehtävää</p>
+            <p>yhteensä {parts.reduce(reducer, 0)} tehtävää</p>
+        </div>
+    )
+}
+
+const Course = ({course}) => {
+    console.log(course);
+    
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     )
 }
 
 const App = () => {
     const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [
-          {
-            name: 'Reactin perusteet',
-            exercises: 10
-          },
-          {
-            name: 'Tiedonvälitys propseilla',
-            exercises: 7
-          },
-          {
+      name: 'Half Stack -sovelluskehitys',
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7,
+          id: 2
+        },
+        {
             name: 'Komponenttien tila',
-            exercises: 14
-          }
-        ]
+            exercises: 14,
+            id: 3
+        },
+        {
+            name: 'Redux',
+            exercises: 7,
+            id: 4
+        }
+      ]
     }
   
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts} />
-    </div>
-  )
-
-}
+    return (
+      <div>
+        <Course course={course} />
+      </div>
+    )
+  }
 
 ReactDOM.render(<App />, document.getElementById('root'))
