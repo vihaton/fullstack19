8a32decd3fe44e054fbc11b22861c1f22ade37b4
Vili <vili.hatonen@helsinki.fi>
2019-01-29 20:46:28 +0200

    2.10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,6 +6,58 @@ const Person = ({person}) => {
   return <p>{person.name} {person.number}</p>
 }
 
+const Filter = (props) => {
+  console.log('render filter', props);
+
+  return (
+    <div>
+    rajaa näytettäviä: <input 
+      value={props.nf}
+      onChange={props.hfc}
+      />
+  </div>
+  )
+}
+
+const PersonForm = (props) => {
+  console.log('personform, lets render', props);
+
+  return (
+    <form onSubmit={props.ae}>
+    <div>
+      nimi: <input 
+        value={props.nn}
+        onChange={props.hnamec}
+        />
+    </div>
+    <div>
+      numero: <input
+        value={props.nnum}
+        onChange={props.hnumc}
+        />
+    </div>
+    <div>
+      <button type="submit">lisää</button>
+    </div>
+  </form>
+  )
+  
+}
+
+const Persons = ({toShow}) => {
+  console.log('lets render persoooooons', toShow);
+  const rows = () => toShow.map(p =>
+    <Person
+      key={p.name}
+      person={p}
+    />
+  )
+
+  return (
+    <div>{rows()}</div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
@@ -15,7 +67,6 @@ const App = () => {
   ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
-  const [showAll, setShowAll] = useState(true)
   const [ newFilter, setNewFilter] = useState('')
 
   const entriesToShow = persons.filter(p => p.name.toUpperCase().includes(newFilter.toUpperCase()))
@@ -51,42 +102,19 @@ const App = () => {
 
   const handleFilterChange = (event) => setNewFilter(event.target.value)
 
-  const rows = () => entriesToShow.map(p =>
-    <Person
-      key={p.name}
-      person={p}
-    />
-  )
-
   return (
     <div>
       <h2>Puhelinluettelo</h2>
-      <div>
-          rajaa näytettäviä: <input 
-            value={newFilter}
-            onChange={handleFilterChange}
-            />
-        </div>
+      <Filter nf={newFilter}
+        hfc={handleFilterChange} />
       <h2>lisää uusi</h2>
-      <form onSubmit={addEntry}>
-        <div>
-          nimi: <input 
-            value={newName}
-            onChange={handleNameChange}
-            />
-        </div>
-        <div>
-          numero: <input
-            value={newNumber}
-            onChange={handleNumberChange}
-            />
-        </div>
-        <div>
-          <button type="submit">lisää</button>
-        </div>
-      </form>
+      <PersonForm ae={addEntry} 
+        nn={newName}
+        hnamec={handleNameChange}
+        nnum={newNumber}
+        hnumc={handleNumberChange}/>
       <h2>Numerot</h2>
-      {rows()}
+      <Persons toShow={entriesToShow} />
     </div>
   )
 
