cf36b49291adb5d2f5ed869e092e940c77cd19bd
Vili <vili.hatonen@helsinki.fi>
2019-02-06 21:45:46 +0200

    first build for backend compatibility

---
+++ osa2/puhelinluettelo/build/precache-manifest.c0e7835f16046c976eb4fad107adb499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b2e5e37017953b2f1ff7",
+    "url": "/static/js/main.b2e5e370.chunk.js"
+  },
+  {
+    "revision": "885a0f92c7c31c74bcab",
+    "url": "/static/js/1.885a0f92.chunk.js"
+  },
+  {
+    "revision": "b2e5e37017953b2f1ff7",
+    "url": "/static/css/main.3b05ae63.chunk.css"
+  },
+  {
+    "revision": "7ab54ecbecc3c47622f6f7cb52dbd555",
+    "url": "/index.html"
+  }
+];
