1fcec592264f2fb8d34d99f7984223536af2ff58
Vili <vili.hatonen@helsinki.fi>
2019-02-12 21:08:12 +0200

    4.5*

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -1,8 +1,33 @@
 const listHelper = require('../utils/list_helper')
+const {blogs, listWithOneBlog} = require('./example_blogs')
 
 test('dummy returns one', () => {
-  const blogs = []
-
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('when list has several blogs', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36)
+  })
+})
+
+describe("favourite blog", () => {
+
+  test('when list has only one blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    console.log(result);
+    
+    expect(result).toEqual(listWithOneBlog[0])
+  })
+
+  test('when list has several blogs', () => {
+    expect(listHelper.favoriteBlog(blogs)).toEqual(blogs[2])
+  })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,25 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = blogs => {
+  const reducer = (sum, item) => {
+    return item.likes
+      ? sum + item.likes
+      : sum + 0
+  }
+  
+  return blogs.length === 0
+    ? 0 
+    : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = blogs => {
+  const indexOfMaxValue = blogs.reduce((iMax, x, i, blogs) => {
+    return x.likes > blogs[iMax].likes ? i : iMax
+  }, 0);
+  return blogs[indexOfMaxValue]
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes, favoriteBlog
 }
