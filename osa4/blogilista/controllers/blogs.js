28a209624d9054126cd8defac04cc9cb89bec49d
Vili <vili.hatonen@helsinki.fi>
2019-02-15 15:36:53 +0200

    4.14*

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,13 +1,34 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const logger = require('../utils/logger')
+const {blogs} = require('../tests/example_blogs')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
+blogsRouter.get('/init', async (req, resp) => {
+  existingBlogs = await Blog.find({})
+  logger.info(`Let's init the DB, we have ${existingBlogs.length} blogs atm and the init file has ${blogs.length}`)
+  let added = 0
+  for (const key in blogs) {
+    if (blogs.hasOwnProperty(key)) {
+      const blog = Blog(blogs[key]);
+      const existing = await Blog.find(blog)
+      if (existing.length === 0) {
+        added += 1
+        logger.info("lets add a blog ", blog)
+        await Blog(blog).save()
+      }
+    }
+  }
+
+  resp.status(200).end(`blogs-app DB successfully initiated; we added ${added}`)
+})
+
+blogsRouter.get('/', async (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
   
 blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
@@ -26,6 +47,23 @@ blogsRouter.post('/', (request, response) => {
     .then(result => {
       response.status(201).json(result)
   })
+})
+
+blogsRouter.delete("/:id", async (req, resp) => {
+  result = await Blog.findByIdAndRemove(req.params.id)
+
+  logger.info('delete succeeded with result:', result)
+  resp.status(204).end()
+})
+
+blogsRouter.put("/:id", async (req, resp) => {
+  logger.info('put with id', req.params.id)
+  const blog = Blog(req.body)
+
+  updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+  
+  logger.info(`updated blog ${updatedBlog}`)
+  resp.json(updatedBlog.toJSON())
 })
 
 module.exports = blogsRouter
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -7,36 +7,3 @@ const server = http.createServer(app)
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
-
-
-// const config = require("./utils/config")
-// const express = require('express')
-// const app = express()
-// const bodyParser = require('body-parser')
-// const cors = require('cors')
-// const Blog = require('./models/blog')
-
-// app.use(cors())
-// app.use(bodyParser.json())
-
-// app.get('/api/blogs', (request, response) => {
-//   Blog
-//     .find({})
-//     .then(blogs => {
-//       response.json(blogs)
-//     })
-// })
-
-// app.post('/api/blogs', (request, response) => {
-//   const blog = new Blog(request.body)
-
-//   blog
-//     .save()
-//     .then(result => {
-//       response.status(201).json(result)
-//     })
-// })
-
-// app.listen(config.PORT, () => {
-//   console.log(`Server running on port ${config.PORT}`)
-// })
