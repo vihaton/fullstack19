61229ba82ab35f2c8c6f7f3a738c9b5dd9250e0a
Vili <vili.hatonen@helsinki.fi>
2019-02-21 19:30:39 +0200

    5.9*

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -238,8 +238,10 @@ const App = () => {
                 handUrlChange={handleUrlChange} />
             </Togglable>
             
-            {blogs.map(blog =>
-              <Blog key={blog.id} blog={blog} handleLike={handleLike} />
+            {blogs
+              .sort((a, b) => b.likes - a.likes)
+              .map(blog =>
+                <Blog key={blog.id} blog={blog} handleLike={handleLike} />
               )}
           </div>
         : ""
