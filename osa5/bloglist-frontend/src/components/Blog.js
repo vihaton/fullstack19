e9de903d0583ddc8fadb22c34a4a4ea5f8b5b7db
Vili <vili.hatonen@helsinki.fi>
2019-02-21 13:54:54 +0200

    5.6*

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+
+const Blog = ({ blog }) => {
+
+  const [visible, setVisible] = useState(false)
+  
+  // const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  
+  const toggleVisibility = () => {
+    // console.log('clicked');
+    setVisible(!visible)
+  }
+
+  // console.log('blog', blog);
+  
+  return (
+    <div style={blogStyle}>
+      <div onClick={toggleVisibility}>
+        {blog.title} {blog.author}
+
+        <div style={showWhenVisible}>
+          <a href={blog.url}>{blog.url}</a> <br></br>
+          {blog.likes} likes 
+          <button onClick={() => console.log('liked')}>
+            Like
+          </button> <br></br>
+          added by {blog.user.name}
+        </div>
+      </div>
+    </div> 
+  )
+}
 
 export default Blog
