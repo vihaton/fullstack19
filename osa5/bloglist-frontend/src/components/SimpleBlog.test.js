d905fdd4d1ca4bb1ae1b36fb54b34cb5ecd9b720
Vili <vili.hatonen@helsinki.fi>
2019-02-22 11:00:34 +0200

    5.14

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,24 +1,40 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 // import { prettyDOM } from 'dom-testing-library'
 
-
-test('renders content', () => {
+const initSBlog = () => {
   const sblog = {
     title: 'Blogi',
     author: 'Jäbä',
     likes: 123
   }
 
-  const component = render(
-    <SimpleBlog blog={sblog} />
-  )
+  return sblog
+}
 
+test('renders content', () => {
+  const component = render(
+    <SimpleBlog blog={initSBlog()} />
+  )
   const titleAuthor = component.getByText('Blogi Jäbä')
   expect(titleAuthor).toBeDefined()
   // console.log(prettyDOM(titleAuthor))
 
   const element = component.getByText('blog has 123 likes')
   expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog={initSBlog} onClick={mockHandler} />
+  )
+
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
