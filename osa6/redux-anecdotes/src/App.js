d313bc71fd7786ad54befa7fc6c662bc8a59384a
Vili <vili.hatonen@helsinki.fi>
2019-02-26 21:55:05 +0200

    6.5*

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -24,7 +24,9 @@ const App = (props) => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
